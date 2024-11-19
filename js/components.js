bb0179609a0dbb243babb4f911e3b2c2e4c302bc
YaninaKramar <yanu_kramar@yahoo.com>
2024-11-19 20:32:18 -0300

    logo

--- js/components.js
+++ js/components.js
@@ -4,7 +4,7 @@ class Components {
         return `
             <nav>
                 <ul class="nav-ul">
-                    <li class="nav-li container-logo" id="li-home-largo"><img class="foto-perfil" src="img/logo.png" alt="logo"><a href="home.html" class="interno">Memorial Chastain Park</a></li>
+                    <li class="nav-li container-logo" id="li-home-largo"><img class="foto-logo" src="img/logo.png" alt="logo"><a href="home.html" class="interno">Memorial Chastain Park</a></li>
                     <li class="nav-li" id="li-home-corto"><a href="home.html" class="interno">MCP</a></li>
                     <li class="nav-li"><a href="turnos.html" class="interno">Pedir Turno</a></li>
                     <li class="nav-li"><a href="salaDeEspera.html" class="interno">Demanda espontánea</a></li>
